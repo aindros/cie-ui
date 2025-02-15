0644c17a2fbbfe9ed357dca8ecaddbcd124b762e
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2025-02-15 12:17:47 +0100

    Migrate to PDF Box
    ghost4j is too old and in some systems doesn't work well.

--- CIEID/src/main/java/it/ipzs/cieid/Firma/PdfPreview.java
+++ CIEID/src/main/java/it/ipzs/cieid/Firma/PdfPreview.java
@@ -7,8 +7,10 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Insets;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -18,10 +20,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
-import org.ghost4j.document.DocumentException;
-import org.ghost4j.document.PDFDocument;
-import org.ghost4j.renderer.RendererException;
-import org.ghost4j.renderer.SimpleRenderer;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
 
 public class PdfPreview {
     private JPanel prPanel;
@@ -29,7 +30,7 @@ public class PdfPreview {
     private String signImagePath;
     private int pdfPageIndex;
     private int pdfNumPages;
-	private List<Image> images;			    
+	private List<Image> images = new ArrayList<>();
 	private JLabel imgLabel;
     private ImageIcon imgIcon;
     private MoveablePicture signImage;
@@ -49,35 +50,36 @@ public class PdfPreview {
 		signImage = new MoveablePicture(signImagePath);
 		imgPanel.add(signImage );
 		imgPanel.add(imgLabel);
-		
-		try {
-			PDFDocument document = new PDFDocument();
-			document.load(new File(filePath));		
-			pdfNumPages = document.getPageCount();
-			System.out.println("Pdf page: " + pdfNumPages);
-		    SimpleRenderer renderer = new SimpleRenderer();
-		    
-		    renderer.setResolution(100);
-		    prPanel.removeAll();
-			images = renderer.render(document);
-			
-			showPreview();
 
-		    
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("PDF File not found");
-			e.printStackTrace();
-		} catch (DocumentException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Document Exception");
-			e.printStackTrace();
-		} catch (RendererException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Renderer Exception");
-			e.printStackTrace();
-		}
+		renderImages(filePath);
+		showPreview();
     }
+
+	public static Image convertToImage(BufferedImage bufferedImage) {
+		/* Set the dimension of the image */
+		int width = bufferedImage.getWidth();
+		int height = bufferedImage.getHeight();
+
+		/* And convert to java.awt.Image */
+		return bufferedImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+	}
+
+	private void renderImages(String pdfFilePath) {
+		prPanel.removeAll();
+
+		try (final PDDocument document = PDDocument.load(new File(pdfFilePath))){
+			pdfNumPages = document.getNumberOfPages();
+			System.out.println("Pdf page: " + pdfNumPages);
+			PDFRenderer pdfRenderer = new PDFRenderer(document);
+			for (int page = 0; page < pdfNumPages; ++page) {
+				BufferedImage bim = pdfRenderer.renderImageWithDPI(page, 300, ImageType.RGB);
+				images.add(convertToImage(bim));
+			}
+		} catch (IOException e){
+			System.err.println("Exception while trying to create pdf document - " + e);
+			throw new RuntimeException();
+		}
+	}
     
     private void showPreview()
     {
