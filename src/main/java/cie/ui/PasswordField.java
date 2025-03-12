bcff8f0ab0bfb42c7f1e38ef62b5d526bcc0ba20
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2025-03-12 12:19:13 +0100

    Fix the behaviour of PIN field
    The last digit, on backspace pression, erase the digit and the
    adjacent digit. This commit fix that behaviour.

--- src/main/java/cie/ui/PasswordField.java
+++ src/main/java/cie/ui/PasswordField.java
@@ -29,53 +29,9 @@ public class PasswordField extends JPanel {
 			passwordFields[i] = passwordField;
 		}
 
-		passwordFields[0].addKeyListener(new PasswordFieldKeyAdapter(0, passwordFields) {
-			@Override
-			public void keyTyped(KeyEvent e) {
-				if (e.getKeyChar() < '0' || e.getKeyChar() > '9') {
-					e.consume();
-				} else {
-					requestFocus(1);
-				}
-			}
-		});
-
-		for (int i = 1; i < passwordFields.length - 1; i++) {
-			JPasswordField passwordFieldBefore = passwordFields[i - 1];
-
-			final int index = i;
-			passwordFields[i].addKeyListener(new PasswordFieldKeyAdapter(index, passwordFields) {
-				@Override
-				public void keyTyped(KeyEvent e) {
-					if (e.getKeyChar() == '\b') {
-						passwordFieldBefore.setText("");
-						requestFocus(index - 1);
-					} else if (e.getKeyChar() < '0' || e.getKeyChar() > '9') {
-						e.consume();
-					} else {
-						requestFocus(index + 1);
-					}
-				}
-			});
+		for (int i = 0; i < passwordFields.length; i++) {
+			passwordFields[i].addKeyListener(new PasswordFieldKeyAdapter(i, passwordFields));
 		}
-
-		final int index = passwordFields.length - 1;
-		JPasswordField passwordFieldBefore = passwordFields[index - 1];
-		passwordFields[index].addKeyListener(new PasswordFieldKeyAdapter(index, passwordFields) {
-			@Override
-			public void keyTyped(KeyEvent e) {
-				if (e.getKeyChar() == '\n' || e.getKeyChar() == '\r') {
-					submit();
-				} else if (e.getKeyChar() == '\b') {
-					passwordFieldBefore.setText("");
-					requestFocus(index - 1);
-				} else if (e.getKeyChar() < '0' || e.getKeyChar() > '9') {
-					e.consume();
-				} else if (passwordFields[index].getPassword().length > 0) {
-					e.consume();
-				}
-			}
-		});
 	}
 
 	public void onSubmit(Runnable action) {
--- src/main/java/cie/ui/PasswordFieldKeyAdapter.java
+++ src/main/java/cie/ui/PasswordFieldKeyAdapter.java
@@ -10,10 +10,51 @@ import java.awt.event.KeyEvent;
 class PasswordFieldKeyAdapter extends KeyAdapter {
 	private final int index;
 	private final JPasswordField[] passwordFields;
+	private boolean filledLastField = false;
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		actionOnArrowKeys(index, e);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (!isLastField() && e.getKeyCode() != KeyEvent.VK_BACK_SPACE) return;
+		filledLastField = passwordFields[index].getPassword().length > 0;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		if (keyTypedOnLastField(e)) return;
+
+		/* If user insert a character in the last field, must be deleted before the cursor can move to the adjacent field */
+		if (e.getKeyChar() == '\b' && !filledLastField) {
+			clearAndFocus(index - 1);
+			return;
+		}
+
+		if (isNotDigit(e.getKeyChar())) {
+			e.consume();
+			return;
+		}
+
+		requestFocus(index + 1);
+	}
+
+	private boolean keyTypedOnLastField(KeyEvent e) {
+		if (!isLastField()) return false;
+
+		if (isNewLine(e.getKeyChar())) {
+			System.out.println("Invocare il submit");
+			return true;
+		}
+
+		if (passwordFields[index].getPassword().length > 0) {
+			e.consume();
+			return true;
+		}
+
+		return false;
 	}
 
 	private void actionOnArrowKeys(int index, KeyEvent e) {
@@ -26,8 +67,35 @@ class PasswordFieldKeyAdapter extends KeyAdapter {
 		requestFocus(keyCode == KeyEvent.VK_RIGHT? index + 1 : index - 1);
 	}
 
+	private boolean outOfRange(int index) {
+		return index < 0 || passwordFields.length <= index;
+	}
+
+	private boolean isLastField() {
+		if (outOfRange(index)) throw new RuntimeException(String.format("index: %s is out of range", index));
+		return index == passwordFields.length - 1;
+	}
+
+	private boolean isNewLine(char c) {
+		return c == '\n' || c == '\r';
+	}
+
 	protected void requestFocus(int index) {
-		if (index < 0 || passwordFields.length <= index) return;
+		if (outOfRange(index)) return;
 		passwordFields[index].requestFocus();
+	}
+
+	protected boolean isNotDigit(char c) {
+		return c < '0' || c > '9';
+	}
+
+	private void clear(int index) {
+		if (outOfRange(index)) return;
+		passwordFields[index].setText("");
+	}
+
+	private void clearAndFocus(int index) {
+		clear(index);
+		requestFocus(index);
 	}
 }
